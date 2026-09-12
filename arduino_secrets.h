274ff991708a3e7428969225509069565fffb40b
Richard <richvern31@gmail.com>
2026-09-12 07:26:15 +0200

    Add OTA password to arduino_secrets.h
    Added OTA password definition for remote access.

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -2,7 +2,9 @@
 #define SECRET_PASS  "MotDePasse1"
 #define SECRET_SSID2 "NomDeVotreReseau2"
 #define SECRET_PASS2 "MotDePasse2"
-
 // Optionnel : décommentez pour ajouter un 3e réseau connu
 // #define SECRET_SSID3 "NomDeVotreReseau3"
 // #define SECRET_PASS3 "MotDePasse3"
+
+// Mode OTA
+#define SECRET_OTA_PASSWORD "MotDePasse_OTA"
